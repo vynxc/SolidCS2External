e496b3da9dddabedd36148b69594b8cfdaed451f
chaycee <svynxc@gmail.com>
2024-02-15 23:15:58 -0500

    remove import

--- SolidCS2External/Utils/RenderablesGetter.cs
+++ SolidCS2External/Utils/RenderablesGetter.cs
@@ -1,16 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using SolidCS2External.ImGuiRendering;
 using SolidCS2External.Interfaces;
 
 namespace SolidCS2External.Utils;
 
 public class RenderablesGetter(IServiceProvider serviceProvider)
 {
-    public T GetService<T>() where T : IRenderable
-    {
-        return serviceProvider.GetRequiredService<T>();
-    }
-
     public List<T> GetAll<T>() where T : IRenderable
     {
         return serviceProvider.GetServices<T>().ToList();
