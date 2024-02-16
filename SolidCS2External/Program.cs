2c619d6408e70328011e5013dd4e2ed35b017832
vynxc <93080410+vynxc@users.noreply.github.com>
2024-02-16 00:29:34 -0500

    Update Program.cs

--- SolidCS2External/Program.cs
+++ SolidCS2External/Program.cs
@@ -4,7 +4,6 @@ using SolidCS2External.Utils;
 
 var serviceCollection = new ServiceCollection();
 Startup.ConfigureService(serviceCollection);
-serviceCollection.AddSingleton<RenderablesGetter>(x => new RenderablesGetter(x));
 await using var serviceProvider = serviceCollection.BuildServiceProvider();
 var startup = serviceProvider.GetRequiredService<Startup>();
 await startup.ConfigureAsync();
