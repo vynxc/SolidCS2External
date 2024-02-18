e73519a186c5abd294e1b689fedab9756178a431
grovre <landonvee111@gmail.com>
2024-02-17 22:41:15 -0600

    ExternalValue.cs for dynamic integrated updating
    deref multilevel ptr done the way CE shows it

--- SolidCS2External/Game/Entity/EntityPawn.cs
+++ SolidCS2External/Game/Entity/EntityPawn.cs
@@ -1,9 +1,9 @@
 ﻿using System.Runtime.InteropServices;
+using SolidCS2External.Game.Offsets;
 
 namespace SolidCS2External.Game.Entity;
 
-[StructLayout(LayoutKind.Explicit)]
-public struct EntityPawn
+public class EntityPawn(Memory.Memory memory)
 {
-    [FieldOffset(0x334)] public int Health;
+    public ExternalValue<int> Health = new(memory, 0x0, 0x1);
 }
