9816406ee49bd200e7206353f4646c1311eaaa14
chaycee <svynxc@gmail.com>
2024-02-22 18:34:14 -0500

    clean program cs

--- SolidCS2External/Program.cs
+++ SolidCS2External/Program.cs
@@ -1,10 +1,6 @@
-﻿//using BenchmarkDotNet.Running;
-using Microsoft.Extensions.DependencyInjection;
-//using SolidCS2External.Benchmarks;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SolidCS2External.Startup;
 
-//var summary = BenchmarkRunner.Run<MemoryBenchmark>(); 
-//Console.WriteLine(summary.ToString());
 var serviceCollection = new ServiceCollection();
 Startup.ConfigureService(serviceCollection);
 await using var serviceProvider = serviceCollection.BuildServiceProvider();
